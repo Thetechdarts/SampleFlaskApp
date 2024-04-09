b7c5075db5eb18ff131285c27eca52975f02634b
Thetechdarts <60461534+Thetechdarts@users.noreply.github.com>
2024-04-10 02:46:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+# Create a Flask app instance
+app = Flask (__name__)
+# Define a route
+@app.route("/")
+def hello ():
+        return "<p> Hello, Welcome to flask web app! <p>"
+# Run the app if executed directly
+if __name__ == '__main__':
+        app.run (host = '0.0.0.0')
