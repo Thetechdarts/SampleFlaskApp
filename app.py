25e88861bbddbc6cc801e433dcc24116463ebc43
The Tech Darts <60461534+Thetechdarts@users.noreply.github.com>
2024-04-16 16:20:14 +0530

    Update app.py
    Changes to text display

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 # Define a route
 @app.route("/")
 def hello ():
-        return "<p> Hello, Welcome to flask web app! <p>"
+        return "<p style="text-align:center"> Hello, Welcome to flask web app! <p>"
 # Run the app if executed directly
 if __name__ == '__main__':
         app.run (host = '0.0.0.0')
